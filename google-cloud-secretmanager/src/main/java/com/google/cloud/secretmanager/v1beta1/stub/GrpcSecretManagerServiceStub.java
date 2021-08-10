667befbe0579fed289f93985b55fb17d5145eb9a
yoshi-automation <yoshi-automation@google.com>
2021-08-10 14:38:59 -0700

    chore: release gapic-generator-java v1.0.17
    Committer: @miraleung
    PiperOrigin-RevId: 388499329
    
    Source-Author: Google APIs <noreply@google.com>
    Source-Date: Tue Aug 3 11:12:29 2021 -0700
    Source-Repo: googleapis/googleapis
    Source-Sha: bb0a090d9204110042ab5dee2ce9e06e8071ce54
    Source-Link: https://github.com/googleapis/googleapis/commit/bb0a090d9204110042ab5dee2ce9e06e8071ce54

--- google-cloud-secretmanager/src/main/java/com/google/cloud/secretmanager/v1beta1/stub/GrpcSecretManagerServiceStub.java
+++ google-cloud-secretmanager/src/main/java/com/google/cloud/secretmanager/v1beta1/stub/GrpcSecretManagerServiceStub.java
@@ -654,7 +654,13 @@ public class GrpcSecretManagerServiceStub extends SecretManagerServiceStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
